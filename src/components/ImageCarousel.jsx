853a7fa4cca5e444a10a7c152c8c6ec7bab27c24
Awwal2007 <awwalsaminu9@gmail.com>
2025-09-20 10:41:36 +0100

    sdfdfdf

--- src/components/ImageCarousel.jsx
+++ src/components/ImageCarousel.jsx
@@ -22,7 +22,7 @@ const ImageCarousel = () => {
             src={img}
             alt={`Slide ${index + 1}`}
             loading="lazy"
-            style={{ width: "100%", height: "500px", objectFit: "cover" }}
+            style={{ width: "100%", height: "400px", objectFit: "cover" }}
           />
         </div>
       ))}
