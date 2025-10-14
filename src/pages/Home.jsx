d98879e434012609b2309b32e7ec34e614422271
Awwal2007 <awwalsaminu9@gmail.com>
2025-10-14 12:44:24 +0100

    sgfsh

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -34,7 +34,7 @@ const Home = () => {
   return (
     <>
       <Helmet>
-        <title>Home | Iwo Land</title>
+        <title>Home | Iwo Land - Officila Website</title>
         <meta name="google-site-verification" content="Z8KOXq8ILNK0QJxU6ib1jyh24h7Of2VEKZlagRE35JA" />
         <meta name="robots" content="index, follow" />
         <link rel="canonical" href="https://iwoland.com/" />
