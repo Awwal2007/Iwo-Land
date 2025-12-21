5332c3c1bcf0af11055a7cc5b7bf374e80d8baca
Awwal2007 <awwalsaminu9@gmail.com>
2025-12-21 14:56:49 +0100

    added gallery to admin page

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './css/Admin.css';
 import { useNews } from '../hooks/useNews';
+import { toast } from 'sonner';
 
 const Admin = () => {
   const [events, setEvents] = useState([]);
@@ -8,6 +9,14 @@ const Admin = () => {
   const [expandedRows, setExpandedRows] = useState({});
   const [facebookLink, setFacebookLink] = useState('');
   const [activeTab, setActiveTab] = useState('events');
+  // gallery
+  const [galleryData, setGalleryData] = useState({
+    title: "",
+    gallery: null
+  })
+  const [galleryLoading, setGalleryLoading] = useState(false)
+  const [galleryImages, setGalleryImages] = useState([])
+  const [imageGalleryPreviews, setImageGalleryPreviews] = useState(null)
 
   const [newEvent, setNewEvent] = useState({
     title: '',
@@ -36,6 +45,7 @@ const Admin = () => {
   useEffect(() => {
     fetchNews();
     fetchFacebookLink();
+    fetchGalleryImages();
   }, []);
 
   // Keep local events list synced with news from the hook
@@ -130,6 +140,98 @@ const Admin = () => {
     if (window.FB) window.FB.XFBML.parse();
   }, [facebookPosts]);
 
+
+  // Gallery Images
+
+  const handleGalleryImage = (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+
+    setGalleryData((prev) => ({ ...prev, gallery: file }));
+    setImageGalleryPreviews(URL.createObjectURL(file));
+  };
+
+
+  const baseUrl = import.meta.env.VITE_BASE_URL
+  const token = localStorage.getItem("accessToken")
+
+  const addGalleryImage = async (e) => {
+    e.preventDefault();
+
+    if (!galleryData.title || !galleryData.gallery) {
+      alert("Gallery title and image are required");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("title", galleryData.title);
+    formData.append("gallery", galleryData.gallery);
+
+    try {
+      setGalleryLoading(true)
+      const response = await fetch(`${baseUrl}/gallery`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        body: formData,
+      });
+
+      if (!response.ok) throw new Error("Failed to create Gallery Image");
+
+      const created = await response.json();
+
+      if (created.status === "success") {
+        toast.success("Gallery image created successfully");
+        fetchGalleryImages();
+        setGalleryData({ title: "", gallery: null });
+        setImageGalleryPreviews(null);        
+      }
+    } catch (error) {
+      console.error(error);
+    }finally{
+      setGalleryLoading(false)
+    }
+  };
+
+
+  const fetchGalleryImages = async()=>{
+    try {
+      const res =  await fetch(`${baseUrl}/gallery`)
+      if(!res.ok) throw new Error("Failed to Load gallery images")
+      const data = await res.json()
+      
+      if(data.status === "success"){
+        setGalleryImages(data.gallery)
+      }
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
+
+  const removeGalleryImage = async (id) => {
+    if (window.confirm('Are you sure you want to delete this gallery image?')){
+      try {
+        const res = await fetch(`${baseUrl}/gallery/${id}`,{
+          method: "DELETE",
+          headers: { 'Authorization': `Bearer ${token}` }
+        })
+
+        if(!res.ok) throw new Error("Fail to delete gallery image")
+
+        const deleted = await res.json()
+
+        if(deleted.status === "success"){
+          alert(deleted.message)
+        }
+        fetchGalleryImages()
+      } catch (error) {
+        console.log(error);        
+      }
+    }
+  };
+
   return (
     <div className="admin-container">
       <header className="admin-header">
@@ -146,6 +248,12 @@ const Admin = () => {
             onClick={() => setActiveTab('facebook')}
           >
             Facebook Posts
+          </button>
+          <button 
+            className={activeTab === 'gallery' ? 'nav-btn active' : 'nav-btn'}
+            onClick={() => setActiveTab('gallery')}
+          >
+            Gallery Images
           </button>
         </nav>
       </header>
@@ -216,10 +324,10 @@ const Admin = () => {
               </div>
               
               <div className="preview-row">
-                {imagePreviews.mainImage && <div className="preview-container"><img loading='lazy' src={imagePreviews.mainImage} alt='mainImage' className="preview" /><span>Main</span></div>}
-                {imagePreviews.image1 && <div className="preview-container"><img loading='lazy' src={imagePreviews.image1} alt='image1' className="preview" /><span>Image 1</span></div>}
-                {imagePreviews.image2 && <div className="preview-container"><img loading='lazy' src={imagePreviews.image2} alt='image2' className="preview" /><span>Image 2</span></div>}
-                {imagePreviews.image3 && <div className="preview-container"><img loading='lazy' src={imagePreviews.image3} alt='image3' className="preview" /><span>Image 3</span></div>}
+                {imagePreviews.mainImage && <div className="preview-container"> <img loading='lazy' src={imagePreviews.mainImage} alt='mainImage' className="preview" /><span>Main</span></div>}
+                {imagePreviews.image1 && <div className="preview-container">  <img loading='lazy' src={imagePreviews.image1} alt='image1' className="preview" /><span>Image 1</span></div>}
+                {imagePreviews.image2 && <div className="preview-container">  <img loading='lazy' src={imagePreviews.image2} alt='image2' className="preview" /><span>Image 2</span></div>}
+                {imagePreviews.image3 && <div className="preview-container">  <img loading='lazy' src={imagePreviews.image3} alt='image3' className="preview" /><span>Image 3</span></div>}
               </div>
               
               <button disabled={creating} type="submit" className="submit-btn">
@@ -251,7 +359,7 @@ const Admin = () => {
                     {events.map((ev) => (
                       <tr key={ev._id}>
                         <td>
-                          <img loading='lazy' src={ev.mainImage} alt={ev.title} className="thumb" />
+                            <img loading='lazy' src={ev.mainImage} alt={ev.title} className="thumb" />
                         </td>
                         <td>
                           {expandedRows[ev._id]?.title ? ev.title : truncate(ev.title, 20)}
@@ -277,9 +385,9 @@ const Admin = () => {
                           )}
                         </td>
                         <td className="extra-images">
-                          {ev.image1 && <img loading='lazy' src={ev.image1} alt={ev.title} className="thumb" />}
-                          {ev.image2 && <img loading='lazy' src={ev.image2} alt={ev.title} className="thumb" />}
-                          {ev.image3 && <img loading='lazy' src={ev.image3} alt={ev.title} className="thumb" />}
+                          {ev.image1 &&   <img loading='lazy' src={ev.image1} alt={ev.title} className="thumb" />}
+                          {ev.image2 &&   <img loading='lazy' src={ev.image2} alt={ev.title} className="thumb" />}
+                          {ev.image3 &&   <img loading='lazy' src={ev.image3} alt={ev.title} className="thumb" />}
                         </td>
                         <td>
                           <button onClick={() => removeEvent(ev._id)} className="delete-btn">Delete</button>
@@ -328,6 +436,58 @@ const Admin = () => {
                     ></div>
                   </div>
                   <button onClick={() => removeFacebookPost(post._id)} className="delete-btn">Delete Post</button>
+                </div>
+              ))
+            )}
+          </div>
+        </section>
+      )}
+
+      {activeTab === 'gallery' && (
+        <section className="facebook-section">
+          <h2>Manage Gallery Images</h2>
+          
+          <form onSubmit={addGalleryImage} className="facebook-form">
+            <div className="form-group">
+              <label htmlFor="galleryTitle">Gallery Title</label>
+              <input
+                type="text"
+                name='title'
+                id="galleryTitle"
+                value={galleryData.title}
+                onChange={(e)=> setGalleryData((prev)=> ({...prev, title: e.target.value}))}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="gallery">Upload Gallery Image</label>
+              <input
+                type="file"
+                accept='image/*'
+                id="gallery"
+                name='gallery'
+                // value={galleryData.gallery}
+                onChange={handleGalleryImage}
+                required
+              />
+            </div>
+            {imageGalleryPreviews &&
+              <img src={imageGalleryPreviews} alt="uploaded image" />
+            }
+            <button disabled={galleryLoading} type="submit" className="submit-btn">{galleryLoading ? "Adding...." : "Add Gallery Image"}</button>
+          </form>
+          
+          <div className="facebook-posts-grid">
+            {galleryImages.length === 0 ? (
+              <div className="no-data">No Gallery image added yet.</div>
+            ) : (
+              galleryImages.map((post) => (
+                <div key={post._id} className="facebook-post-card">
+                  <div className="fb-post-container">
+                    <div>{post.title}</div>
+                    <img height={100} src={post.gallery} alt={post.title} />
+                  </div>
+                  <button onClick={() => removeGalleryImage(post._id)} className="delete-btn">Delete Post</button>
                 </div>
               ))
             )}
